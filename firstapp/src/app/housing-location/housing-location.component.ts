feba465337e043a44426535363aaf3dec80924ef
dev.santhoz001@gmail.com <dev.santhoz001@gmail.com>
2024-05-16 00:58:47 +0530

    done upto data binding

---
+++ firstapp/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Housinglocation } from '../housinglocation';
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  templateUrl: './housing-location.component.html',
+  styleUrl: './housing-location.component.css'
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!: Housinglocation;
+}
